79ec40804101b98412bb4fb37544e5fd9d096589
Cyano Prem <cyano.prem@gmail.com>
2022-07-22 19:37:01 +0530

    Add more boxes with hover effect

--- javascript.js
+++ javascript.js
@@ -1,8 +1,15 @@
-const square = document.querySelector('.square');
+const container = document.querySelector('.container');
 
-square.addEventListener('mouseover', hover);
-
-function hover () {
-    square.classList.add('hover');
+for(let i = 0; i < 256; i++) {
+    let div = document.createElement('div');
+    div.classList.add('square');
+    container.appendChild(div);
 }
+
+const square = document.querySelectorAll('.square');
+
+square.forEach(hover => hover.addEventListener('mouseover', function hover(e) {
+    e.target.classList.add('hover');
+}));
+
 
